4eefbc2bf50a870f7a3db1892e0bd179fd9da5d8
GS <gsfernandes81@gmail.com>
2021-10-03 13:45:49 +0100

    Added args, switched to a build Class, switched to pypy

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import csv
+import argparse
 from typing import Dict, List
-import pprint
+from enum import Enum
 
 # Positions of id, name, d2_class, exotic, slot, mob, res, rec, dis, int, str in the CSV Row
 CSV_ROW_DEFN = [2, 0, 7, 4, 5, 27, 28, 29, 30, 31, 32]
 
-TIER_LIMIT = 30
+# Argument parsing
+parser = argparse.ArgumentParser(description='List armor to dismantle')
+parser.add_argument('--mods', action='store_true', help='Enable mods')
+parser.add_argument('--tier', type=int, help='Minimum build tier')
+args = parser.parse_args()
+BASE_MODS_ENABLED = args.mods
+if args.tier is None:
+    TIER_LIMIT = 31
+    if BASE_MODS_ENABLED:
+        TIER_LIMIT += 5
+else:
+    TIER_LIMIT = args.tier
+
+
+class Stat(Enum):
+    MOBILITY = 0
+    RESILIENCE = 1
+    RECOVERY = 2
+    DISCIPLINE = 3
+    INTELLECT = 4
+    STRENGTH = 5
 
 
 class Armor:
@@ -126,42 +147,95 @@ class Armor:
         return cls(*params)
 
 
-def build_is_valid(build: List[Armor]):
-    if not all([item.d2_class == build[0].d2_class for item in build]):
-        return False
-    number_of_exotics = 0
-    for armor in build:
-        if armor.is_exotic:
-            number_of_exotics += 1
-    if number_of_exotics > 1:
-        return False
-    return True
+class Build(List):
+    def __init__(self, armor_list: List[Armor], mods_used: int = 0):
+        self.extend(armor_list)
+        self.mods_used = mods_used
+        self.stats = [0, 0, 0, 0, 0, 0]
+        for armor in self:
+            self.stats[Stat.MOBILITY.value] += armor.mobility
+            self.stats[Stat.RESILIENCE.value] += armor.resilience
+            self.stats[Stat.RECOVERY.value] += armor.recovery
+            self.stats[Stat.DISCIPLINE.value] += armor.discipline
+            self.stats[Stat.INTELLECT.value] += armor.intellect
+            self.stats[Stat.STRENGTH.value] += armor.strength
+        
+    def add_mods(build):
+        for idx, tier in enumerate(build.stats):
+            # This formula checks if a tier can benefit from a +5 mod
+            if tier % 10 > 5 and build.mods_used < 5:
+                build.mods_used += 1
+                build.stats[idx] += 5
+        for mod_no in range(5 - build.mods_used):
+            for idx, tier in enumerate(build.stats):
+                if tier < 100:
+                    build.stats[idx] += 10
+                    build.mods_used += 1
+                    break
+        return build
 
+    def calculate_tier(build):
+        # Calculate effective tiers
+        individual_tiers = [0, 0, 0, 0, 0, 0,]
+        for idx in range(len(build.stats)):
+            if BASE_MODS_ENABLED:
+                build.add_mods()
+            individual_tiers[idx] = build.stats[idx] // 10
+            individual_tiers[idx] = (
+                10 if individual_tiers[idx] > 10 else individual_tiers[idx]
+            )
+        total = sum(individual_tiers)
+        # Account for armor masterworks
+        total += 6
+        return total
+    
+    def mark(build):
+        for armor in build:
+            armor.mark = True
 
-def calc_build_tier(build: List[Armor]):
-    individual_tiers = [0, 0, 0, 0, 0, 0]
-    for armor in build:
-        individual_tiers[0] += armor.mobility
-        individual_tiers[1] += armor.resilience
-        individual_tiers[2] += armor.recovery
-        individual_tiers[3] += armor.discipline
-        individual_tiers[4] += armor.intellect
-        individual_tiers[5] += armor.strength
-    # Calculate effective tiers
-    for idx in range(len(individual_tiers)):
-        individual_tiers[idx] = individual_tiers[idx] // 10
-        individual_tiers[idx] = (
-            10 if individual_tiers[idx] > 10 else individual_tiers[idx]
-        )
-    total = sum(individual_tiers)
-    # Account for armor masterworks
-    total += 5
-    return total
+    def is_valid(build):
+        if not all([item.d2_class == build[0].d2_class for item in build]):
+            return False
+        number_of_exotics = 0
+        for armor in build:
+            if armor.is_exotic:
+                number_of_exotics += 1
+        if number_of_exotics > 1:
+            return False
+        return True
 
+class try_achieve_build(list):
+    def __init__(self, build: List[Armor], mods_used: int = 0):
+        self.extend(build)
+        self.mods_used = mods_used
 
-def mark_build(build: List[Armor]):
-    for armor in build:
-        armor.mark = True
+    def _general_stat_func(self, stat_name: str, stat: int):
+        while True:
+            gulf = self[int(Stat(stat_name.upper()))] - stat
+            if gulf > 5:
+                self.mods_used += 1
+                self[int(Stat(stat_name.upper()))] += 10
+            else:
+                break
+        return self
+
+    def mobility(self, mobility: int):
+        return self._general_stat_func("mobility")
+
+    def resilience(self, resilience: int):
+        return self._general_stat_func("resilience")
+
+    def recovery(self, recovery: int):
+        return self._general_stat_func("recovery")
+
+    def discipline(self, discipline: int):
+        return self._general_stat_func("discipline")
+
+    def intellect(self, intellect: int):
+        return self._general_stat_func("intellect")
+
+    def strength(self, strength: int):
+        return self._general_stat_func("strength")
 
 
 armor_lists: List[List[Armor]] = [[], [], [], [], []]
@@ -195,19 +269,15 @@ armor_lists[4].append(
     Armor(0, "Class Item", "Titan", False, "Class Item", 0, 0, 0, 0, 0, 0)
 )
 
-build_counter = 0
-unmarked_armor_counter = 0
-
 for zero in armor_lists[0]:
     for one in armor_lists[1]:
         for two in armor_lists[2]:
             for three in armor_lists[3]:
                 for four in armor_lists[4]:
-                    build = [zero, one, two, three, four]
-                    if build_is_valid(build):
-                        if calc_build_tier(build) >= TIER_LIMIT:
-                            mark_build(build)
-                            build_counter += 1
+                    build = Build([zero, one, two, three, four])
+                    if build.is_valid():
+                        if build.calculate_tier() >= TIER_LIMIT:
+                            build.mark()
 
 # Save at least one exotic
 exotic_list = []
@@ -232,10 +302,12 @@ for armor in armor_lists[0] + armor_lists[1] + armor_lists[2] + armor_lists[3]:
         armor.mark = True
 
 query = str()
+unmarked_armor_counter = 0
 for armor in armor_lists[0] + armor_lists[1] + armor_lists[2] + armor_lists[3]:
     if not armor.mark:
         unmarked_armor_counter += 1
         query += " or id:" + str(armor.id)
 query = query[4:]
 
-print(query)
+print("\n" + query + "\n")
+print("Number of junk armor pieces:", unmarked_armor_counter)
